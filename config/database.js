0d1e73323ceaa215ed8da52ab5289f2af49cda0a
Luiz Fernando <84513178+glitchy-luiz@users.noreply.github.com>
2022-09-13 11:37:09 -0300

    Delete config directory

--- config/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const conexao = async()=>{
-    const atlas = await mongoose.connect('mongodb+srv://userac1:ac1@fiaptecnico.yqfxr.mongodb.net/ac')
-}
-
-module.exports = conexao
